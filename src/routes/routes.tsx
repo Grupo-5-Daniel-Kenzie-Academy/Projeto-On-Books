e036ef24196a2fb4f5cbba848359083165ad1f3f
JUorlando <110193846+JUorlando@users.noreply.github.com>
2023-01-03 10:42:50 -0300

    feat: dashboard page

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
+import { Dashboard } from "../pages/Dashboard";
 import { Home } from "../pages/Home";
 import { Login } from "../pages/Login";
 import { Register } from "../pages/Register";
@@ -7,9 +8,10 @@ import { Register } from "../pages/Register";
 export const RoutesComponents = () => {
   return (
     <Routes>
-      <Route path="/" element={<Login />} />
+      <Route path="/" element={<Dashboard />} />
       <Route path="register" element={<Register />} />
-      <Route path="dashboard" element={<Home />} />
+      <Route path="home" element={<Home />} />
+      <Route path="login" element={<Login />} />
     </Routes>
   );
 };
