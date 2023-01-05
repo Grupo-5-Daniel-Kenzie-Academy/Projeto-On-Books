139f82926f246f7e0bd753f069eca1365daca1bc
thales <thalesvrpb21@gmail.com>
2023-01-05 16:51:54 -0300

    feat: modificado css home, login e register page, responsivos também

--- src/styles/Global.ts
+++ src/styles/Global.ts
@@ -32,9 +32,27 @@ export const GlobalStyle = createGlobalStyle`
   :root{
     --color-modal: #9DD8F9;
     --grey-1: #E0DDDD;
+    --grey-2: #5F5F65;
+    --grey-3: #DCDBDB;
+    --grey-4: #EAEAFD;
+    --grey-5: #A5A0A0;
+    --color-primary: #181EBE;
+    --color-primary-70: rgba(24, 29, 190, 0.7);
+    --color-primary-50: rgba(24, 29, 190, 0.5);
+    --white: #FFF;
 
+
+    --radius-4: 4px;
     --radius-6: 6px;
+    --radius-8: 8px;
     --radius-10: 10px;
+    --radius-20: 20px;
+
+    --rem-08: 0.8rem;
+    --rem-09: 0.9rem;
+    --rem-11: 1.1rem;
+    --rem-16: 1.6rem;
+
   }
 
 `;
