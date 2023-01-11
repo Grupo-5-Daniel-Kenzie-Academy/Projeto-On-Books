69b43210dd70675bbe419f46cbe69ee47f3e75c4
Pedro Alcantara <pedroalcanm@hotmail.com>
2023-01-11 10:10:22 -0300

    Fix: corrige css

--- src/components/ListRead/style.ts
+++ src/components/ListRead/style.ts
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 
 export const Licard = styled.li`
+  min-width: 200px;
   width: 200px;
   height: 280px;
 
@@ -8,6 +9,37 @@ export const Licard = styled.li`
   flex-direction: column;
   align-items: center;
   justify-content: center;
+
+  .divInfoBO {
+    width: 100%;
+    height: 90px;
+
+    display: flex;
+    flex-direction: column;
+
+    align-items: center;
+    justify-content: center;
+
+    border-top: 2px solid var(--gray-3);
+
+    gap: 5px;
+
+    p {
+      font-weight: 700;
+      font-size: 16px;
+      line-height: 29px;
+      color: var(--gray-5);
+      width: 100%;
+
+      text-align: center;
+
+      white-space: nowrap;
+      overflow: hidden;
+      text-overflow: ellipsis;
+    }
+  }
+
+  text-align: center;
 
   gap: 15px;
 
